1849fc1eda669f7f355961d526f217c188daff84
山田かみら <superfishman9@gmail.com>
2019-03-01 10:30:23 +0800

    更新
    修正BUG及產出與前版本相比多的文本，並且會忽略新版本沒有且舊版本存在的文本

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import os
 from os import listdir
 from os.path import isfile
 import re
+import json
+
 
 old_path = '.\\old'
 new_path = '.\\new'
 old_localisation_file_path = '.\\old_localisation_file'
 old_files = []
-regex = r"(.*(?=\s)) \"(.*)\"$"
+regex = r'\s*([\w\d:\.]*)\s*(.*)'
 raw_local_content = []
-
+record_dict = {}
 files = [f for f in listdir(old_path) if isfile(os.path.join(old_path, f))]
 
 for f in files:
     raw_json = {}
     raw_local_json = {}
+    record_list = []
     old_localisation_file = os.path.join(old_localisation_file_path, f)
     with open(os.path.join(old_path, f), 'r', encoding='utf-8-sig') as raw:
         raw_content = raw.read().splitlines()
@@ -29,14 +32,21 @@ for f in files:
         if result is None:
             continue
         raw_json[result.group(1)] = result.group(2)
+        record_list.append(result.group(1))
     for i in raw_local_content:
         result = re.search(regex, i)
         if result is None:
             continue
-        raw_json[result.group(1)] = result.group(2)
-    print(raw_json)
+        if result.group(1) in record_list:
+            raw_json[result.group(1)] = result.group(2)
+            record_list.remove(result.group(1))
+
+    record_dict[f] = record_list
+
     with open(os.path.join(new_path, f), 'w', encoding='utf-8-sig') as writer:
         writer.write(f'{title}\n')
         for k, v in raw_json.items():
-            writer.write(f' {k} "{v}"\n')
-
+            writer.write(f' {k} {v}\n')
+    print(record_list)
+with open('differance.json', 'w' , encoding='utf-8-sig') as outfile:
+    json.dump(record_dict, outfile)
